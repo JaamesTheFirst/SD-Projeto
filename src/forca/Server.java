63eb5f875e4e7bb2659bf054b53c25077dd360a7
Rita Frazão <118269400+Riitafrazao@users.noreply.github.com>
2026-04-15 19:08:29 +0100

    Refactor Server class for player management and game start
    Refactored Server class to handle player connections and game initialization directly in main method.

--- src/forca/Server.java
+++ src/forca/Server.java
@@ -1,29 +1,46 @@
 import java.net.*;
-
-import forca.MainThread;
-
 import java.io.*;
+import java.util.ArrayList;
 
 public class Server {
-    private ServerSocket ss;
-    private Socket s;
-    private MainThread c;
-    public Server() {
-        try {
-            ss = new ServerSocket(5432);
+    public static void main(String[] args) {
+        ArrayList<Player> jogadores = new ArrayList<>();
+        
+        try (ServerSocket ss = new ServerSocket(5432)) {
+            System.out.println("Servidor iniciado. À espera de dois ou mais jogadores ...");
+            try {
+                while (jogadores.size() < 4) {
+                    Socket s = ss.accept();
+                    int id = jogadores.size() + 1;
+                    MainThread t = new MainThread(s, id);
+                    jogadores.add(new Player(id, t));
+                    System.out.println("Jogador " + id + " entrou.");
+                    if(jogadores.size() == 2) {
+                    System.out.println("Agora que dois jogadores entraram, o lobby vai esperar mais 20 segundos por mais jogadores...");
+                    ss.setSoTimeout(20000);
+            }
+                }
+            } catch (SocketTimeoutException e) {
+                System.out.println("Tempo de lobby esgotado (20s).");
+            }
+
+            if (jogadores.size() >= 2) {
+                System.out.println("O Jogo vai começar com " + jogadores.size() + " jogadores!");
+                
+                Game jogo = new Game();
+                for (Player p : jogadores) {
+                    p.getThreadDeComunicacao().setJogo(jogo); 
+                }
+
+                String palavraSorteada = Ler.sortearPalavra();
+                jogo.inicioJogo(jogadores, palavraSorteada);
+                
+            } else {
+                System.out.println("Jogadores insuficientes. Tente novamente.");
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        while (true) {
-            try {
-                s = ss.accept();
-                c = new MainThread(s);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Server jogo = new Server();
     }
 }
