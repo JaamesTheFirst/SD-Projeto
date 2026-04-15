f55dc88c184b1184d0785a6ee2274a815b612fb4
Rita Frazão <118269400+Riitafrazao@users.noreply.github.com>
2026-04-15 19:07:26 +0100

    Refactor MainThread to extend Thread and manage socket

--- src/forca/MainThread.java
+++ src/forca/MainThread.java
@@ -1,21 +1,57 @@
-package forca;
 import java.net.*;
 import java.io.*;
 
-public class MainThread extends MainThread {
-    private Socket S;
-    public MainThread ( Socket s) {
+public class MainThread extends Thread {
+    private Socket socket;
+    private int idJogador;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Game jogo; 
+    
+    public MainThread (Socket s, int id) {
         super();
-        S = s;
-        start();
-    }
-    public void run () {
+        this.socket = s;
+        this.idJogador = id;
         try {
-            ObjectOutputStream os = new ObjectOutputStream( S.getOutputStream());
-            os.writeObject ("A data e hora do sistema: " + new java.util.Date());
-            os.flush();
-        }catch ( IOException e) {
-            e.printStackTrace();
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    } 
+    
+    public void setJogo(Game jogo) {
+        this.jogo = jogo;
+    }
+
+    public void enviarMensagem(String msg) {
+        if (out != null) out.println(msg);
+    }
+
+    @Override
+    public void run() {
+        try {
+            enviarMensagem(Protocolo.getWelcome(idJogador, 4));
+
+            String mensagemCliente;
+            while ((mensagemCliente = in.readLine()) != null) {
+                
+                String[] partes = mensagemCliente.trim().split("\\s+");
+                
+                if (partes.length >= 2) {
+                    
+                    if (partes[0].equals(Protocolo.guess) && jogo != null) {
+                        String tentativa = partes[1].toUpperCase(); 
+                        
+                        jogo.processarJogada(this.idJogador, tentativa); 
+                    }
+                } else {
+                    System.out.println("Comando inválido recebido do jogador " + idJogador);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Jogador " + idJogador + " desconectou-se.");
         }
     }
 }
